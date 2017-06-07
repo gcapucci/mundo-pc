6e47fa2c2bd250968b3e4fafe4304bec4f6f2a46
Juan Ignacio Cuiule <juanignaciocuiule@gmail.com>
2017-06-06 23:23:13 -0300

    tratando de arreglar conflictos de merge

--- mundopc.js
+++ mundopc.js
@@ -1,43 +1,11 @@
-<<<<<<< HEAD
 $("a[href^='#']").click(function(e) {
 	e.preventDefault();
-
 	var position = $($(this).attr("href")).offset().top;
-
-	
-    
-$("body, html").animate({
+	$("body, html").animate({
 		scrollTop: position -200
 	}, 1000 );
 });
 
- 
 $(".accordion").on("click", ".accordion-header", function() {
  	$(this).toggleClass("active").next().slideToggle();
- });
-
-
-
-
-
-=======
-$("a[href^='#']").click(function(e) {
-	e.preventDefault();
-
-	var position = $($(this).attr("href")).offset().top;
-
-	$("body, html").animate({
-		scrollTop: position
-	}, 1000 );
 });
-
- 
-$(".accordion").on("click", ".accordion-header", function() {
- 	$(this).toggleClass("active").next().slideToggle();
- });
-
-
-
-
-
->>>>>>> e4b70b4cdbac560de9850034c22d1aee6065d8c0
