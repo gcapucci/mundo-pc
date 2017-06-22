e275ef115e75551d2027d89b6ce871251c49a657
Lauti Monaco <lauti.monaco@hotmail.com>
2017-06-21 22:56:27 -0300

    imagenes

---
+++ js/mundopc.js
@@ -0,0 +1,11 @@
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	var position = $($(this).attr("href")).offset().top;
+	$("body, html").animate({
+		scrollTop: position -200
+	}, 1000 );
+});
+
+$(".accordion").on("click", ".accordion-header", function() {
+ 	$(this).toggleClass("active").next().slideToggle();
+});
